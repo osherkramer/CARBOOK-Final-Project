2e74c624e1e5162536bd3abe110711f311582a7e
CarBook <carbook.israel@gmail.com>
2017-05-11 18:21:09 +0300

    Adding function for DBHelper and adding new classes

--- FinalProjectV1/FinalProjectV1/Models/HistoryItem.cs
+++ FinalProjectV1/FinalProjectV1/Models/HistoryItem.cs
@@ -5,12 +5,21 @@ using System.Web;
 
 namespace FinalProjectV1.Models
 {
-    public class Treatment
+    public class HistoryItem
     {
-        public int CarID { get; set; }
-        public DateTime CareDate { get; set; }
         public int TreatmentID { get; set; }
+        public string CarNumber { get; set; }
         public string CareType { get; set; }
+        public DateTime Date { get; set; }
+        public string[] Treatment { get; set; }
         public int KM { get; set; }
+        public string GarageName { get; set; }
+
+
+        public HistoryItem()
+        {
+            CarNumber = null;
+            Date = DateTime.Now;
+        }
     }
 }

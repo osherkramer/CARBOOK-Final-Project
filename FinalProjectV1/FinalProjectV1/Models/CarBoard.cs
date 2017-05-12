a46a3dca9949d3d2a1ae15df7476a2739c38ad8e
CarBook <carbook.israel@gmail.com>
2017-05-12 14:18:39 +0300

    add Models/Advertisement.cs && Models/CarBoard.cs

---
+++ FinalProjectV1/FinalProjectV1/Models/CarBoard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectV1.Models
+{
+    public class CarBoard
+    {
+        public string ProductName { get; set; }
+        public string CommericalAlias { get; set; } //Model
+        public string EngineCapacity { get; set; }
+        public string Year { get; set; }
+        public string Yad { get; set; }
+        public string Gaer { get; set; }
+        public string FuelType { get; set; }
+        public string Ownership { get; set; }
+        public string KM { get; set; }
+
+        public string ABS { get; set; }
+        public string Roof { get; set; }
+        public string CarNumber { get; set; }
+        public string AirBags { get; set; }
+        public string AC { get; set; }
+
+        public string Description { get; set; }
+        public string SellerName { get; set; }
+        public string Tel { get; set; }
+        public string Pic { get; set; }
+     
+        
+        
+         
+        
+      
+   
+   
+     
+        
+        
+   
+     
+ 
+      
+   
+     
+     
+
+       
+       
+    
+       
+     
+       
+      
+     
+       
+       
+       
+    }
+}

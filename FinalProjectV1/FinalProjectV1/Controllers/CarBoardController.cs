62234a9bd040bfd4d1531eb33c04ef2f0c1e9041
Moshit Davidson <moshit2192@gmail.com>
2017-05-14 15:15:02 +0300

    fixing problem at CarBoardController

--- FinalProjectV1/FinalProjectV1/Controllers/CarBoardController.cs
+++ FinalProjectV1/FinalProjectV1/Controllers/CarBoardController.cs
@@ -1,18 +1,21 @@
-﻿using FinalProjectV1.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinalProjectV1.Models;
+using FinalProjectV1.Helpers;
 
 namespace FinalProjectV1.Controllers
 {
     public class CarBoardController : Controller
     {
+
         // GET: CarBoard
         public ActionResult Index()
         {
-            List<Advertisment> ads = new List<Advertisment>();
+           
+            List<Advertisement> ads = new List<Advertisement>();
             ads = DBHelper.returnAdvertisments();
 
 
